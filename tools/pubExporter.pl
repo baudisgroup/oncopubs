42101c6b91fe180e6da12b907e57e2fa27233d6d
mbaudis <m@baud.is>
2017-10-12 14:43:56 +0200

    biocharacteristics from samples

--- tools/pubExporter.pl
+++ tools/pubExporter.pl
@@ -4,13 +4,14 @@ use Data::Dumper;
 use JSON::XS;
 use MongoDB;
 use MongoDB::MongoClient;
+use Term::ProgressBar;
 use YAML::XS;
 
 STDOUT->autoflush(1);
 
 my %args        =   @ARGV;
 
-$args{'-out' }          ||=	'..';
+$args{'-out' }          ||=	 '..';
 $args{'-coll'}          ||=  'publications_web';
 $args{'-filetype'}      ||=  'yaml';            # alternative: json
 $args{'-update'}        ||=  'y';
@@ -20,26 +21,7 @@ my $currentPubs =   $args{'-out' }.'/current';
 my $inPubs      =   $args{'-out' }.'/incoming';
 my $excludePubs =   $args{'-out' }.'/excluded';
 
-my $checkPaths  =   1;
-foreach ($currentPubs, $inPubs, $excludePubs) {
-  if (! -d $_) {
-    print <<END;
-
-An existing "$_" directory has to exist.
-
-END
-    $checkPaths   =   -1;
-  }
-
-  if ($checkPaths < 1) {
-    print <<END;
-
-Please provide a valid "-out" root directory.
-
-END
-
-    exit;
-}}
+_checkPathsExit($currentPubs, $inPubs, $excludePubs);
 
 print <<END;
 
@@ -76,11 +58,18 @@ foreach (grep{ /$filetype/ } readdir(DIR)) {
 }
 close DIR;
 
-# reformatting the publication entries & exporting them to "-out"
-# using the
-foreach my $pgPMID (@$pgPMIDs) {
 
-  my $pub       =   $dbcoll->find_one( { PMID => qr/^$pgPMID$/ } );
+my $amga4ghbs   =   MongoDB::MongoClient->new()->get_database('arraymap_ga4gh')->get_collection('biosamples');
+my $pgga4ghbs   =   MongoDB::MongoClient->new()->get_database('progenetix_ga4gh')->get_collection('biosamples');
+
+
+# reformatting the publication entries & exporting them
+
+my $progress_bar = Term::ProgressBar->new(@$pgPMIDs);
+
+for my $i (0..$#{ $pgPMID }) {
+
+  my $pub       =   $dbcoll->find_one( { PMID => qr/^$pgPMID->[$i]$/ } );
 
   foreach (keys %$pub) {
     if ($pub->{$_} !~ /\w/)     { delete $pub->{$_} }
@@ -106,6 +95,7 @@ foreach my $pgPMID (@$pgPMIDs) {
       samples_wgs       =>  1 * $pub->{NO_WGS},
       biosamples        =>  1 * $pub->{SAMPLENO},
     },
+    cancertypes =>  [],
     notes       =>  $pub->{PUBNOTE},
   };
 
@@ -119,6 +109,7 @@ foreach my $pgPMID (@$pgPMIDs) {
   $pubDump->{external_identifiers}      =   [];
   if ($pub->{PMID} =~ /^|\:\d{6,10}$/) {
     push(@{$pubDump->{external_identifiers}}, 'pubmed:'.$pub->{PMID}) }
+
   foreach my $accession (grep{ /\w\w\w/ } split(',', $pub->{ACCESSION})) {
     $accession  =~  s/[\w\.\:]\://g;
     if ($accession =~ /^GSE\d{2,10}$/) {
@@ -129,12 +120,37 @@ foreach my $pgPMID (@$pgPMIDs) {
       push(@{$pubDump->{external_identifiers}}, 'arrayexpress:'.$accession) }
   }
 
-  if ($pub->{CANCERTYPE} =~ /^seer\s(21049)\:\s*?(\w.*?)$/) {
-    $pubDump->{cancertype}      =   {
-      term_id           =>  'seer:'.$1,
-      term_label        =>  $2,
-    };
+
+  my %bioOntologies        =   ();
+
+  # cancertype currently using SEER
+  if ($pub->{CANCERTYPE} =~ /^seer\s(\d{5})\:\s*?(\w.*?)$/) {
+    $bioOntologies{ 'seer:'.$1 } =   $2 }
+
+  # from samples ...
+  foreach my $coll ($amga4ghbs, $pgga4ghbs) {
+    my $cursor	=		$coll->find( { "external_identifiers.identifier" => qr/^(?:(?:pubmed)|(?:pmid)\:)?$pub->{PMID}$/ } )->fields( { bio_characteristics => 1 } );
+    my @samples	=		$cursor->all;
+    foreach my $sample (@samples) {
+      foreach my $bioC (@{$sample->{bio_characteristics}}) {
+        foreach my $ontology (@{$bioC->{ontology_terms}}) {
+          $bioOntologies{ $ontology->{term_id} } =   $ontology->{term_label};
+        }
+      }
+    }
   }
+
+  foreach (sort keys %bioOntologies) {
+    push(
+      @{ $pubDump->{cancertypes} },
+      {
+        term_id           =>  $_,
+        term_label        =>  $bioOntologies{ $_ },
+      }
+    );
+  }
+
+
 
   if ($pub->{geo_data}->{geo_json}->{coordinates}->[1] =~ /^\-?\d+?(\.\d)?\d*?$/) {
     $pubDump->{geo_data}        =  {
@@ -169,6 +185,9 @@ foreach my $pgPMID (@$pgPMIDs) {
       YAML::XS::DumpFile($pubFile, $pubDump);
     }
   }
+
+  $progress_bar->update($i);
+
 }
 
 if ($args{'-cleanup'} =~ /y/) {
@@ -182,6 +201,30 @@ if ($args{'-cleanup'} =~ /y/) {
       `$cmd`;
     }
   }
+}
+
+
+
+
+################################################################################
+
+sub _checkPathsExit {
+
+  my ($currentPubs, $inPubs, $excludePubs)      =   @_;
+  my $checkPaths  =   1;
+  foreach ($currentPubs, $inPubs, $excludePubs) {
+    if (! -d $_) {
+      print <<END;
+
+An existing "$_" directory has to exist.
+
+END
+      $checkPaths =   -1;
+    }
+    if ($checkPaths < 1) { exit }
+
+  }
+
 }
 
 
